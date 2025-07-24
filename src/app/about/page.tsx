c3dc923ba219da6e1875bcd5d9885d632bbdab4c
Bhanuka-botheju <wpbhanukabotheju@gmail.com>
2025-07-25 02:34:34 +0530

    finishing bio section in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function page() {
   return (
     <div className='container max-w-7xl mx-auto py-20'>
-      <h1 className='text-4xl font-bold mb-8 text-center'>About Me</h1>
+      <h1 className='text-4xl font-bold mb-8 text-center text-gray-600 dark:text-gray-300'>About Me</h1>
+      <section>
+        <p className='text-lg text-secondary max-w-3xl mx-auto text-center'>
+          I'm a Computer Science and Engineering undergraduate at the University of Moratuwa, 
+          currently specializing in Data Science. 
+          I’m passionate about solving real-world problems through data and actively seeking a Data Science internship opportunity
+        </p>
+      </section>
     </div>
   )
 }
